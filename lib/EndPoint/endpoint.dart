c3b5f3439eab176ebc9e87ed4bc35a5c4238a55d
meraj15 <khanmeraj1542005@gmail.com>
2025-01-30 23:53:13 +0530

    change the endpoint

--- lib/EndPoint/endpoint.dart
+++ lib/EndPoint/endpoint.dart
@@ -1,6 +1,6 @@
 class APIEndpoint {
-  //  static const String _baseEndPoint = "https://ecommerce-renderer.onrender.com/api";
-   static const String _baseEndPoint = "http://localhost:3000/api";
+   static const String _baseEndPoint = "https://ecommerce-renderer.onrender.com/api";
+  //  static const String _baseEndPoint = "http://localhost:3000/api";
    static const String productGetEndPoint = "$_baseEndPoint/products";
    static const String postNewProduct = "$_baseEndPoint/products";
    static const String updateData = "$_baseEndPoint/products";
